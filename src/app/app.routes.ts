509883bb213bf87791d4722b172678c772b768d9
Harshith Kandula <kandulaharshith@yahoo.com>
2023-12-14 15:43:04 +0530

    update project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ClientmanagementComponent } from './clientmanagement/clientmanagement.component';
+import { ClientComponent } from './client/client.component';
+import { MeetingsmanagementComponent } from './meetingsmanagement/meetingsmanagement.component';
+import { MeetingsComponent } from './meetings/meetings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: AppComponent},
+    {path: 'ClientsList', component: ClientmanagementComponent},
+    {path: 'AddClient', component: ClientComponent},
+    {path: 'MeetingsList', component: MeetingsmanagementComponent},
+    {path: 'AddMeetings', component: MeetingsComponent}
+];
